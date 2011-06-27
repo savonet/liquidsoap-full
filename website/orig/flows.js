2949ad2593c2530bab6a61c97be6494fbd0021a3
Samuel Mimram <smimram@gmail.com>
2011-06-27 20:16:52 +0200

    Map of radios!

--- website/orig/flows.js
+++ website/orig/flows.js
@@ -1,7 +1,25 @@
+var map;
+
+function gen_click_handler(infowindow, maker)
+{
+    return function ()
+    {
+        infowindow.open(map, marker);
+    }
+}
+
 function update_radios(div)
 {
     var div = document.getElementById(div);
     div.innerHTML = "<b>Loading...</b>";
+
+    var latlng = new google.maps.LatLng(48.86, 2.33);
+    var options = {
+        zoom: 1,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById('map'), options);
 
     $.getJSON(
         "http://savonet.rastageeks.org/liqflows.py?fmt=json&cmd=radios",
@@ -14,7 +32,7 @@ function update_radios(div)
             }
             content = "";
             content += "<ul>\n";
-            line = '<li><div class="radio"><a href="WEBSITE" target="_blank">NAME</a> <span class="streams">[STREAMS]</span></div><div class="description">DESCRIPTION (GENRE)</div><div class="metadata">ARTIST &mdash; <span class="title">TITLE</span></div></li>';
+            line = '<li><div class="radio"><a href="WEBSITE" target="_blank">NAME</a> <span class="streams">[STREAMS]</span></div><div class="genre">GENRE</div><div class="description">DESCRIPTION</div><div class="metadata">METADATA</div></li>';
             for (i=0; i < radios.length; i++)
             {
                 r = radios[i];
@@ -23,8 +41,17 @@ function update_radios(div)
                 l = l.replace("WEBSITE",r.website);
                 l = l.replace("DESCRIPTION",r.description);
                 l = l.replace("GENRE",r.genre);
-                l = l.replace("ARTIST",r.artist);
-                l = l.replace("TITLE",r.title);
+                metadata = "";
+                if (r.artist != null)
+                {
+                    metadata += r.artist;
+                    metadata += " ";
+                    if (r.title != null)
+                        metadata += "&mdash; ";
+                }
+                if (r.title != null)
+                    metadata += '<span class="title">'+r.title+'</span>';
+                l = l.replace("METADATA",metadata);
                 streams = "";
                 for (j = 0; j < r.streams.length; j++)
                 {
@@ -35,6 +62,19 @@ function update_radios(div)
                 }
                 l = l.replace("STREAMS",streams);
                 content += l + "\n";
+                if ((r.latitude != null) && (r.longitude != null))
+                {
+                    latlng = new google.maps.LatLng(r.latitude, r.longitude);
+                    marker = new google.maps.Marker({
+			position: latlng,
+			map: map,
+			title: r.name
+		    });
+                    infowindow = new google.maps.InfoWindow({
+                        content: r.name
+                    });
+                    google.maps.event.addListener(marker, 'click', gen_click_handler(infowindow, marker));
+                }
             }
             content += "</ul>\n";
             div.innerHTML = content;
