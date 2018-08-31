dcf2f0d2b1a3fe40b9d080b43d1c1c8880527ecc
Romain Beauxis <toots@rastageeks.org>
2018-08-30 23:19:50 -0500

    Add cry-windows to win32 files.

--- docker/build-win32.sh
+++ docker/build-win32.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-IMAGE=liquidsoap-win32
+IMAGE=savonet/liquidsoap-win32
 VERSION=$1
 
+docker build -f Dockerfile.win32-deps -t savonet/liquidsoap-win32-deps . 
 docker build --no-cache -f Dockerfile.win32 -t ${IMAGE} --build-arg LIQUIDSOAP_VERSION=$VERSION .
 id=$(docker create ${IMAGE})
 docker cp $id:/tmp/liquidsoap-$VERSION.zip .
