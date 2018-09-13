0b8fc34933e4b6ae5c63f364910473750e1cff88
Romain Beauxis <toots@rastageeks.org>
2018-09-13 18:48:03 -0500

    Update win32 build to support both arch.

--- docker/build-win32.sh
+++ docker/build-win32.sh
@@ -1,11 +1,32 @@
 #!/bin/sh
 
-IMAGE=savonet/liquidsoap-win32
 BUILD=$1
 RELEASE=$2
+SYSTEM=$3
+OCAML_VERSION=$4
 
-docker build -f Dockerfile.win32-deps -t savonet/liquidsoap-win32-deps . 
-docker build -f Dockerfile.win32 -t ${IMAGE} --build-arg RELEASE=$RELEASE --build-arg BUILD=$BUILD .
+if [ -z "${SYSTEM}" ]; then
+  SYSTEM="x64"
+fi
+
+if [ -z "${OCAML_VERSION}" ]; then
+  OCAML_VERSION=4.07.0
+fi
+
+BASE_IMAGE="ocamlcross/windows-${SYSTEM}-base:${OCAML_VERSION}"
+DEPS_IMAGE="savonet/liquidsoap-win32-deps-${SYSTEM}"
+IMAGE="savonet/liquidsoap-win32-${SYSTEM}"
+
+if [ "${SYSTEM}" = "x64" ]; then
+  HOST="x86_64-w64-mingw32.static"
+else
+  HOST="i686-w64-mingw32.static"
+fi
+
+docker build -f Dockerfile.win32-deps -t ${DEPS_IMAGE} \
+  --build-arg IMAGE=${BASE_IMAGE} .
+docker build -f Dockerfile.win32 -t ${IMAGE} --build-arg RELEASE=${RELEASE} \
+  --build-arg IMAGE=${DEPS_IMAGE} --build-arg HOST=${HOST} --build-arg BUILD=${BUILD} .
 id=$(docker create ${IMAGE})
-docker cp $id:/tmp/liquidsoap-$BUILD.zip .
-docker rm -v $id
+docker cp ${id}:/tmp/liquidsoap-${BUILD}.zip .
+docker rm -v ${id}
