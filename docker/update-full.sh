6b8a9f3d912f3ab12db7223507f03b31f04ef1b6
Romain Beauxis <toots@rastageeks.org>
2019-05-28 16:49:13 +0200

    Don't reset image history on full.

--- docker/update-full.sh
+++ docker/update-full.sh
@@ -1,14 +1,4 @@
 #!/bin/sh -e
 
-BUILD_IMAGE=savonet/liquidsoap-full-with-history
-EXPORTED_IMAGE=savonet/liquidsoap-full
-
-docker build --no-cache -t ${BUILD_IMAGE} -f Dockerfile.full .
-
-id=$(docker create ${BUILD_IMAGE})
-
-docker export $id | docker import - ${EXPORTED_IMAGE}
-
-docker rm -v $id
-
-docker push ${EXPORTED_IMAGE}
+docker build --no-cache -t savonet/liquidsoap-full -f Dockerfile.full .
+docker push savonet/liquidsoap-full
