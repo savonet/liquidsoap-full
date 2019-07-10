6bfe9aacb635a869890a9bb704f40fbb087e8ebd
Romain Beauxis <toots@rastageeks.org>
2019-07-10 01:52:35 -0500

    Add argument for base distribution image.

--- docker/update-deps.sh
+++ docker/update-deps.sh
@@ -1,9 +1,17 @@
 #!/bin/sh -e
 
-BUILD_IMAGE=savonet/liquidsoap-deps-with-history
-EXPORTED_IMAGE=savonet/liquidsoap-deps
+BASE_IMAGE=$1
 
-docker build -t ${BUILD_IMAGE} -f Dockerfile.deps .
+if test -z "${BASE_IMAGE}"; then
+  BASE_IMAGE=debian:testing
+fi
+
+TAG=`echo ${BASE_IMAGE} | sed -e 's#/#_#g' | sed -e 's#:#_#g'`
+BUILD_IMAGE=savonet/liquidsoap-deps-with-history:${TAG}
+EXPORTED_IMAGE=savonet/liquidsoap-deps:${TAG}
+
+
+docker build -t ${BUILD_IMAGE} --build-arg BASE_IMAGE="${BASE_IMAGE}" -f Dockerfile.deps .
 
 id=$(docker create ${BUILD_IMAGE})
 
