9a58c2b6de6fdd6291c8cbbeb7bc3649ffe4b944
Romain Beauxis <toots@rastageeks.org>
2019-07-27 12:31:45 -0700

    Updated module(s).

--- docker/update-deps.sh
+++ docker/update-deps.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-BASE_IMAGE=$1
+export BASE_IMAGE=$1
 
 if test -z "${BASE_IMAGE}"; then
   BASE_IMAGE=debian:testing
@@ -10,8 +10,10 @@ TAG=`echo ${BASE_IMAGE} | sed -e 's#/#_#g' | sed -e 's#:#_#g'`
 BUILD_IMAGE=savonet/liquidsoap-deps-with-history:${TAG}
 EXPORTED_IMAGE=savonet/liquidsoap-deps:${TAG}
 
+export OS=`echo ${BASE_IMAGE} | cut -d':' -f 1`
+export DISTRIBUTION=`echo ${BASE_IMAGE} | cut -d':' -f 2`
 
-docker build -t ${BUILD_IMAGE} --build-arg BASE_IMAGE="${BASE_IMAGE}" -f Dockerfile.deps .
+docker build -t ${BUILD_IMAGE} --build-arg BASE_IMAGE --build-arg OS --build-arg DISTRIBUTION -f Dockerfile.deps .
 
 id=$(docker create ${BUILD_IMAGE})
 
