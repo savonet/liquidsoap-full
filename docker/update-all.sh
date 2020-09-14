b4751904f52ecbed6c9cea8360ee8ef5b16f07d8
Romain Beauxis <toots@rastageeks.org>
2020-09-14 09:17:38 -0500

    Prune before build.

--- docker/update-all.sh
+++ docker/update-all.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -e
 
 for image in debian:testing debian:stable ubuntu:eoan ubuntu:focal; do
+  docker system prune -f;
   docker pull $image;
   ./update-deps.sh $image;
   ./update-ci.sh $image;
-  docker system prune -f
 done;
