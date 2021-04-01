e6aa90b4bc3f6f4c8f7a909dce8ee7ab81c23a87
Romain Beauxis <toots@rastageeks.org>
2021-04-01 09:16:26 -0500

    Screen, add ubuntu groovy

--- docker/update-all.sh
+++ docker/update-all.sh
@@ -1,8 +1,5 @@
 #!/bin/sh -e
 
-for image in debian:testing debian:stable ubuntu:eoan ubuntu:focal; do
-  docker system prune -f;
-  docker pull $image;
-  ./update-deps.sh $image;
-  ./update-ci.sh $image;
+for image in debian:testing debian:stable ubuntu:groovy ubuntu:focal; do
+  screen -dmS $image /bin/sh -c "./update-deps.sh $image && ./update-ci.sh $image"
 done;
