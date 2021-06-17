ff1c64e2789dabb7be7cc75ce4b9fc9322a90054
Romain Beauxis <toots@rastageeks.org>
2021-06-16 19:42:27 -0500

    Update update-all.sh

--- docker/update-all.sh
+++ docker/update-all.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -e
 
+screen -dmS alpine /bin/sh -c "./build-alpine-deps.sh && ./update-ci.sh alpine"
+
 for image in debian:testing debian:stable ubuntu:groovy ubuntu:focal; do
   screen -dmS $image /bin/sh -c "./update-deps.sh $image && ./update-ci.sh $image"
 done;
