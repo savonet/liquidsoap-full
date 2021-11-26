d10752b79d710c2d7506004d0ba897c7ced9eb13
Romain Beauxis <toots@rastageeks.org>
2021-11-26 12:42:41 +0100

    Update ci for armhf.

--- docker/update-ci.sh
+++ docker/update-ci.sh
@@ -1,20 +1,23 @@
 #!/bin/sh -e
 
 BASE_IMAGE=$1
+ARCHITECTURE=2
 
 if test -z "${BASE_IMAGE}"; then
   BASE_IMAGE=debian:testing
 fi
 
-if [ "${BASE_IMAGE}" = "arm32v7/debian:bullseye" ]; then
-  ARCHITECTURE=armhf
-  DOCKER_PLATFORM="--platform linux/arm/v7"
-else
+if test -z "${ARCHITECTURE}"; then
   ARCHITECTURE=`dpkg --print-architecture`
-  DOCKER_PLATFORM=
+fi
+
+if [ "${ARCHITECTURE}" = "armhf" ]; then
+  DOCKER_BUILD="buildx build --platform linux/arm/v7"
+else
+  DOCKER_BUILD=build
 fi
 
 TAG=`echo ${BASE_IMAGE} | sed -e 's#/#_#g' | sed -e 's#:#_#g'`_$ARCHITECTURE
 
-docker build --no-cache ${DOCKER_PLATFORM} --build-arg TAG="${TAG}" -t savonet/liquidsoap-ci:${TAG} -f Dockerfile.ci .
+docker ${DOCKER_PLATFORM} --no-cache --build-arg TAG="${TAG}" -t savonet/liquidsoap-ci:${TAG} -f Dockerfile.ci .
 docker push savonet/liquidsoap-ci:${TAG}
