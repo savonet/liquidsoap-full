66e40a4dd806876e59b52ead3ae9b82d810604e9
Romain Beauxis <toots@rastageeks.org>
2023-06-25 10:52:02 -0500

    Update releases.

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -3,10 +3,10 @@
 screen -dmS alpine /bin/sh -c "./build-alpine-ci.sh"
 
 if uname -a | grep aarch64 >/dev/null; then
-  screen -dmS debian-armhf -c "./build-ci.sh debian:bookworm true"
+  screen -dmS debian-armhf -c "./build-ci.sh debian:trixie true"
   screen -dmS alpine-armhf -c "./build-alpine-ci.sh alpine true"
 fi
 
-for image in debian:bullseye debian:bookworm ubuntu:jammy ubuntu:focal; do
+for image in debian:bookworm debian:trixie ubuntu:jammy ubuntu:lunar; do
   screen -dmS $image /bin/sh -c "./build-ci.sh $image"
 done;
