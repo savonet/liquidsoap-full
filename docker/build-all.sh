a7757ba577dfdd514020fc4b9a98af1f57f752be
Romain Beauxis <toots@rastageeks.org>
2025-09-01 10:31:08 -0500

    Also switch oracular to plucky.

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -3,7 +3,7 @@
 for ocaml in 4.14.2; do
   screen -dmS alpine-$ocaml /bin/sh -c "./build-alpine-ci.sh $ocaml"
 
-  for image in debian:forky debian:trixie ubuntu:noble ubuntu:oracular; do
+  for image in debian:forky debian:trixie ubuntu:noble ubuntu:plucky; do
     screen -dmS $image-$ocaml /bin/sh -c "./build-ci.sh $image $ocaml"
     screen -dmS $image-5.2.0+ox /bin/sh -c "./build-ox.sh $image 5.2.0+ox"
   done;
