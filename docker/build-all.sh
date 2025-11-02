0f126c253fa0c8fa30d661bb19d3224f9e08866b
Romain Beauxis <toots@rastageeks.org>
2025-11-02 10:46:05 -0600

    Bring this back.

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -3,7 +3,7 @@
 for ocaml in 4.14.2 5.4.0; do
   screen -dmS alpine-$ocaml /bin/sh -c "./build-alpine-ci.sh $ocaml"
 
-  for image in debian:bookworm debian:trixie ubuntu:noble ubuntu:oracular; do
+  for image in debian:forky debian:trixie ubuntu:noble ubuntu:plucky; do
     screen -dmS $image-$ocaml /bin/sh -c "./build-ci.sh $image $ocaml"
   done;
 done;
