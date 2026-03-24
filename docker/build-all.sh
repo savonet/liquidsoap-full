5339efe3af8e46c5147150cf2cb5b4024a533238
Romain Beauxis <romain.beauxis@gmail.com>
2026-03-23 22:00:35 -0500

    Remove CI docker scripts, moved to liquidsoap repo.

--- docker/build-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-for ocaml in 4.14.2 5.4.0; do
-  screen -dmS alpine-$ocaml /bin/sh -c "./build-alpine-ci.sh $ocaml"
-
-  for image in debian:forky debian:trixie ubuntu:resolute ubuntu:plucky; do
-    screen -dmS $image-$ocaml /bin/sh -c "./build-ci.sh $image $ocaml"
-  done;
-done;
--- docker/build-alpine-ci.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-
-export OCAML_VERSION=$1
-TAG=alpine
-BUILD_IMAGE=ghcr.io/savonet/liquidsoap:ci-${TAG}-${OCAML_VERSION}
-
-depot build --push --platform linux/amd64,linux/arm64 --build-arg OCAML_VERSION -f Dockerfile.alpine-ci . -t ${BUILD_IMAGE}
